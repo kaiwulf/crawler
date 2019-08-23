853a8a2392a13241d043e83a6404f50f55f586cd
kaism <eigenspaces@gmail.com>
2019-08-23 13:12:26 -0400

    first commit

---
+++ crawler.py
@@ -0,0 +1,16 @@
+from bs4 import BeutifulSoup
+
+class search:
+
+    def __init__(url=None, key_words=None):
+        self.url = url
+        self.key_words = key_words
+
+
+class crawler:
+
+    def __init__():
+        pass
+
+def main():
+    
