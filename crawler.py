f317e516f8b820ebf284b9e66a423309ff485e44
kaiwulf <eigenspaces@gmail.com>
2020-11-04 15:28:09 -0500

    need to sort out details

--- crawler.py
+++ crawler.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 from csv import DictReader, DictWriter
 
 # https://css-tricks.com/snippets/css/a-guide-to-flexbox/
+# Using sample program from below to learn more about python webscrapping
 # https://dzone.com/articles/webscraping-with-python-beautiful-soup-and-urllib3
 
 class search:
@@ -29,18 +30,27 @@ for i in range(1,51):
     res = req.request('GET', url)
     soup = BeautifulSoup(res.data, 'html.parser')
     contents = soup.find_all(class_='product_pod')
+    input(type(soup.find_all()))
+    titles = []
+    for j in soup.find_all():
+        titles.append(j['title'])
+    pounds = []
+    c = []
+    for i in contents:
+        c.append(i.find().get_text())
+        for number in c:
+            amount = re.compile('[0-9]+.')
+            num = amount.findall(number)
+            pounds.append(float(''.join(num)))
+    temp = list(map(gdp_to_usd_rounded,pounds))
+    for t in temp:
+        prices.append(t)
+
+    res = dict(zip(titles,prices))
+
+
     if 'title' in i.attrs:
         input("title in attrs")
         titles.append(i['title'])
     pounds = []
     c = []
-    # for i in contents:
-    #     c.append(i.find().get_text())
-    #     for number in c:
-    #         amount = re.compile('[0-9]+.')
-    #         num = amount.findall(number)
-    #         pounds.append(float(''.join(num)))
-    #         for i in soup.find_all():
-    #             if 'title' in i.attrs:
-    #                 input("title in attrs")
-    #                 titles.append(i['title'])
